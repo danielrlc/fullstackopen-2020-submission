c367da58dd1f65dcf6c019a24b30ba216b5ad47b
Daniel Clarke <daniel@danielrlc.com>
2020-12-05 00:42:23 +0200

    1.13*: anecdotes step2

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,19 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (props) => {
-  const [selected, setSelected] = useState(0);
-  const selectAnecdote = () =>
-    setSelected(Math.floor(Math.random() * anecdotes.length));
-
-  return (
-    <div>
-      <p>{props.anecdotes[selected]}</p>
-      <button onClick={selectAnecdote}>Random anecdote</button>
-    </div>
-  );
-};
-
 const anecdotes = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -22,5 +9,35 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 ];
+
+const App = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(0);
+  const [votesRegister, setVotesRegister] = useState(
+    new Array(anecdotes.length).fill(0),
+  );
+
+  const changeAnecdote = () =>
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+
+  const addVote = () => {
+    const updatedVotesRegister = votesRegister.map((item, position) => {
+      if (position === selected) {
+        return item + 1;
+      } else {
+        return item;
+      }
+    });
+    setVotesRegister(updatedVotesRegister);
+  };
+
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votesRegister[selected]} votes</p>
+      <button onClick={addVote}>Vote</button>
+      <button onClick={changeAnecdote}>Change anecdote</button>
+    </div>
+  );
+};
 
 ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
