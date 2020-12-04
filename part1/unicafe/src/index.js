37f88e23a40fa5812b4dfc64e1f376fedaca8cfb
Daniel Clarke <daniel@danielrlc.com>
2020-12-04 15:36:05 +0200

    fixed console error for 1.11*: uncafe step6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,18 +7,20 @@ const Statistics = ({ good, neutral, bad }) => {
       <>
         <h2>Statistics</h2>
         <table>
-          <Statistic text="good" value={good} />
-          <Statistic text="neutral" value={neutral} />
-          <Statistic text="bad" value={bad} />
-          <Statistic text="all" value={good + neutral + bad} />
-          <Statistic
-            text="average"
-            value={(good - bad) / (good + neutral + bad) || 0}
-          />
-          <Statistic
-            text="positive"
-            value={(good / (good + neutral + bad)) * 100 || 0}
-          />
+          <tbody>
+            <Statistic text="good" value={good} />
+            <Statistic text="neutral" value={neutral} />
+            <Statistic text="bad" value={bad} />
+            <Statistic text="all" value={good + neutral + bad} />
+            <Statistic
+              text="average"
+              value={(good - bad) / (good + neutral + bad) || 0}
+            />
+            <Statistic
+              text="positive"
+              value={(good / (good + neutral + bad)) * 100 || 0}
+            />
+          </tbody>
         </table>
       </>
     );
