050e1f09c00451f4b52041325c9ab0ecafab3c6e
Daniel Clarke <daniel@danielrlc.com>
2020-12-06 22:27:05 +0200

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [search, setSearch] = useState('');
@@ -42,6 +37,14 @@ const App = () => {
       )
     : persons;
 
+  useEffect(
+    () =>
+      axios
+        .get('http://localhost:3001/persons')
+        .then((response) => setPersons(response.data)),
+    [],
+  );
+
   return (
     <div>
       <h1>Phonebook</h1>
@@ -55,7 +58,7 @@ const App = () => {
         newNumber={newNumber}
         handleNumberChange={handleNumberChange}
       />
-      
+
       <h2>Numbers</h2>
       <Persons personsToShow={personsToShow} />
     </div>
